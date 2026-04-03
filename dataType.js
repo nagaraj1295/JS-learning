1ee07c399ce1727d5100ea0cf265907b9c677693
NAGARAJ C <rjnaga123@gmail.com>
2026-04-03 21:16:07 +0530

    Add number data type examples
    Add sample code demonstrating JavaScript number primitives and special numeric values. Introduces pi and prime variables, Infinity and -Infinity, NaN, logs their values and typeof results, and computes a simple sum to illustrate numeric operations.

--- dataType.js
+++ dataType.js
@@ -1,1 +1,17 @@
 // number data type
+let pi = 3.14;
+let prime = 345;
+console.log(pi, prime);
+console.log(typeof pi,typeof prime);
+
+let infi = Infinity;
+let ngo = -Infinity;
+console.log(infi,ngo);
+console.log(typeof infi,typeof ngo);
+
+let temp = NaN;
+console.log(temp);
+console.log(typeof temp);
+
+let sum = pi + prime;
+console.log(sum);
